723ab0474ab4dc0e103846a4c57ec381ab70b39e
JupriGH <pabuaran@gmail.com>
2023-10-28 22:31:28 +0700

    updates

--- lib/actor.php
+++ lib/actor.php
@@ -2,8 +2,7 @@
 namespace apify;
 
 /* The main class of the SDK, through which all the actor operations should be done. */
-class Actor
-{
+class Actor {
 	/*
 	Create an Actor instance.
 
@@ -13,18 +12,17 @@ class Actor
 	Args:
 		config (Configuration, optional): The actor configuration to be used. If not passed, a new Configuration instance will be created.
 	*/	
-	
-    function __construct($config=null) {
-        $this->config = $config ?? new Configuration();
+	function __construct($config=null) {
+		$this->config = $config ?? new Configuration();
 		$this->apify_client = $this->new_client();
 		$this->event_manager = new EventManager($this->config);
-		
+
 		$this->_is_initialized = false;
 		$this->_is_exiting = false;
 		$this->_send_system_info_interval_task = null;
 		$this->_send_persist_state_interval_task = null;
 		$this->_was_final_persist_state_emitted = false;
-    }
+	}
 	function new_client() {
 	}
 	function get_input() {
